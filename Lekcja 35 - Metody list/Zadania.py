8ad281da0312e261ff6772c8a257ce9ffb1d08d6
Miszel16 <a.plachmiowicz16@gmail.com>
2025-12-14 13:35:25 +0100

    14.12

---
+++ Lekcja 35 - Metody list/Zadania.py
@@ -0,0 +1,25 @@
+# Zadania 
+
+# 1 Stwórz 2 listy składające się z 3 liczb każda
+lista1 = [1,2,3]
+lista2 = [4,5,6]
+
+
+# 2 Połącz stworzone wcześniej listy
+lista1.extend(lista2)
+print(lista1)
+
+
+# 3 Usuń elementy z indeksami 2 i 5 , który element należy usunąć najpierw?
+lista1.pop(5)
+lista1.pop(2)
+print(lista1)
+
+
+# 4 Usuń największą i najmniejszą liczbę z listy (zakładamy że nie znamy)
+# 5 Dodaj liczbę do listy
+# 6 Posortuj listę
+# 7 Utwórz kopię listy
+# 8 Odwróć kolejność elementów w kopii
+# 9 Dodaj do każdej wartości w pierwszej listy 1, a w drugiej odejmij 1
+# 10 Wyświetl obie listy
